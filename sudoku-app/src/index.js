c9d82d8d819c6a5afa08a195bf8cf0cc79c69a2d
kai-nikaxu <kai@nikaxu.com>
2025-11-04 15:44:53 -0800

    Catogrize styles

--- sudoku-app/src/index.js
+++ sudoku-app/src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "./styles/button.css";
+import "./styles/sudoku.css";
+import "./styles/utils.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
